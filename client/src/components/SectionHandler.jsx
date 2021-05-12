95785f99f014fd1c9647f2827a22fa303b65d1f9
Nenninger Manuel <qxz0q17@europe.bmw.corp>
2021-05-12 08:10:57 +0200

    cleaned orderstructure

---
+++ client/src/components/SectionHandler.jsx
@@ -0,0 +1,70 @@
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
+import Grow from '@material-ui/core/Grow';
+import "./SectionHandler.css";
+import SectionTwo from "./SectionTwo";
+
+function SectionHandler() {
+  let [visibleSectionTwo, visibleSectionThree, visibleSectionFour] = [false];
+
+  return (
+    <div className="scroll-container">
+    <section>
+      <h2>Websites are like business cards on the web, make sure it's a good one</h2>
+    </section>
+    <section>
+      <VisibilitySensor>
+        {
+          ({isVisible}) =>
+          <Grow
+              in={visibleSectionTwo
+                ? true
+                : isVisible} onEnter={() => {
+                visibleSectionTwo = true;
+              }}
+                style={{
+                transformOrigin: '0 0 0'
+              }} {...(isVisible ? { timeout: 700 } : {})}>
+              <SectionTwo />
+            </Grow>
+        }
+      </VisibilitySensor>
+    </section>
+    <section>
+      <VisibilitySensor>
+        {
+          ({isVisible}) => <Grow in={visibleSectionThree
+                ? true
+                : isVisible} onEnter={() => {
+                visibleSectionThree = true;
+              }} style={{
+                transformOrigin: '0 0 0'
+              }} {...(isVisible ? { timeout: 700 } : {})}>
+              <h2>Section 3</h2>
+            </Grow>
+        }
+      </VisibilitySensor>
+    </section>
+    <section>
+      <VisibilitySensor>
+        {
+          ({isVisible}) => <Grow in={visibleSectionFour
+                ? true
+                : isVisible} onEnter={() => {
+                visibleSectionFour = true;
+              }} style={{
+                transformOrigin: '0 0 0'
+              }} {...(isVisible ? { timeout: 700 } : {})}>
+              <h2>Section 4</h2>
+            </Grow>
+        }
+      </VisibilitySensor>
+    </section>
+  </div>
+  );
+}
+
+export default SectionHandler;
---
+++ client/src/components/SectionTwo.jsx
@@ -0,0 +1,12 @@
+
+
+
+function SectionTwo() {
+  return (
+    <div>
+      <h2>Section 2</h2>
+    </div>
+  );
+}
+
+export default SectionTwo;
