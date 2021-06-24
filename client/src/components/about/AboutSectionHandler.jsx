1c84467ed3c743ead866f580594a603fe335d89d
Manuel <manuel.nenninger@web.de>
2021-06-24 06:37:38 +0200

    begin sectionone about

--- client/src/components/about/AboutSectionHandler.jsx
+++ client/src/components/about/AboutSectionHandler.jsx
@@ -1,13 +1,29 @@
-
+import "./AboutSectionHandler.css";
 
 function AboutSectionHandler() {
-  return (
-    <>
-      <section className="About-SectionOne">
-        <h1>Hello World</h1>
-      </section>
-    </>
-  );
+return (
+<>
+  <section className="About-SectionOne">
+    <div className="row About-SectionOne-content-wrapper">
+      <div className="col-md-6 col-12">
+        <div className="About-SectionOne-content-wrapper-text">
+          <div className="About-SectionOne-content-wrapper-titel">
+            <h3>Hello, I´m Manuel</h3>
+          </div>
+          <div className="About-SectionOne-content-wrapper-content">
+            <h4>I am a mechanical engineer and fullstack developer from munich</h4>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6 col-12">
+        <div className="About-SectionOne-content-wrapper-image">
+            <img src="/pictures/Platzhalter.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
+</>
+);
 }
 
 export default AboutSectionHandler;
